a69ff5161303e9a1d76bdef2f939a3773a740dc6
Yaroslav Zozulya <my.dev.stories@gmail.com>
2022-03-23 17:42:39 +0300

    notification

--- src/js/module_11/notification.js
+++ src/js/module_11/notification.js
@@ -1,1 +1,27 @@
 import '../../sass/main.scss';
+
+const NOTIFICATION_DELAY = 3000;
+
+const refs = {
+  notification: document.querySelector('.js-alert'),
+};
+
+refs.notification.addEventListener('click', onNotificationClick);
+
+showNotification();
+
+function onNotificationClick() {
+  hideNotification();
+}
+
+function showNotification() {
+  refs.notification.classList.add('is-visible');
+  setTimeout(() => {
+    hideNotification();
+  }, NOTIFICATION_DELAY);
+}
+
+function hideNotification() {
+  refs.notification.classList.remove('is-visible');
+  console.log('test');
+}
