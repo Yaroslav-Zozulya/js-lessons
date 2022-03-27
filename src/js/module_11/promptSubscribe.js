bf92d6f3ee5d3ae3d0fa41ea68d9e97eb249e5da
Yaroslav Zozulya <my.dev.stories@gmail.com>
2022-03-27 17:15:35 +0300

    prompt subscriptions + BNS

--- src/js/module_11/promptSubscribe.js
+++ src/js/module_11/promptSubscribe.js
@@ -1,17 +1,35 @@
 import '../../sass/main.scss';
+import BSN from 'bootstrap.native';
+
+const refs = {
+  modal: document.querySelector('#subscription-modal'),
+  subscribeBtn: document.querySelector('button[data-subscribe]'),
+};
 
 const PROMPT_DELAY = 1000;
 const MAX_PROMPT_ATTEMPTS = 3;
-
 let promptCounter = 0;
 let hasSubscribed = false;
 
-const intervalId = setInterval(() => {
+const modal = new BSN.Modal('#subscription-modal');
+
+refs.modal.addEventListener('hide.bs.modal', openModal);
+refs.subscribeBtn.addEventListener('click', onSubscribeBtnClick);
+
+openModal();
+
+function openModal() {
   if (promptCounter === MAX_PROMPT_ATTEMPTS || hasSubscribed) {
-    clearInterval(intervalId);
-    console.log('Хватит мучать юзера :)');
     return;
   }
-  console.log('Подпишись на рассылку! - ' + Date.now());
-  promptCounter += 1;
-}, PROMPT_DELAY);
+
+  setTimeout(() => {
+    modal.show();
+    promptCounter += 1;
+  }, PROMPT_DELAY);
+}
+
+function onSubscribeBtnClick() {
+  hasSubscribed = true;
+  modal.hide();
+}
