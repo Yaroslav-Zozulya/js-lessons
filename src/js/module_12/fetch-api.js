9bb5c087bca17c970657b4d1f4dabdaaf4a9ce26
Yaroslav Zozulya <my.dev.stories@gmail.com>
2022-04-09 13:50:50 +0300

    pokemon search

--- src/js/module_12/fetch-api.js
+++ src/js/module_12/fetch-api.js
@@ -1,1 +1,46 @@
 import '../../sass/main.scss';
+
+const pokemonCard = document.querySelector('.js-pokemon-card');
+const searchForm = document.querySelector('.js-search-form');
+const searchBtn = document.querySelector('.js-search-text');
+
+searchForm.addEventListener('submit', onSearch);
+
+function onSearch() {
+  event.preventDefault();
+
+  const pokemonId = searchBtn.value;
+
+  fetchPokemon(pokemonId)
+    .then(renderPokemonCard)
+    .catch(error => console.log(error));
+
+  searchForm.reset();
+}
+
+function fetchPokemon(pokemonId) {
+  return fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`).then(response => {
+    return response.json();
+  });
+}
+
+function renderPokemonCard({ abilities, sprites, name, weight, height }) {
+  const ability = abilities
+    .map(ability => ` <li class="list-group-item">${ability.ability.name}</li>`)
+    .join(' ');
+
+  const markup = `<div class="card">
+        <div class="card-img-top"><img src="${sprites.front_default}" alt="" /></div>
+        <div class="card-body">
+          <h2 class="card-title">Имя: ${name}</h2>
+          <p class="card-text">Вес:${weight}кг</p>
+          <p class="card-text">Рост:${height}м</p>
+          <p class="card-text">Умения</p>
+          <ul class="list-group">
+             ${ability}
+          </ul>
+        </div>
+      </div>`;
+
+  pokemonCard.innerHTML = markup;
+}
